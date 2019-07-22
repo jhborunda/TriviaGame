b5ad44bf11e65e49833ba0b7fb5d8987bca49b81
Jose Borunda <jose.h.borunda@gmail.com>
2019-07-21 21:34:08 -0600

    started JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var question1 = "The NCAA Football Bowl Subdivision, Divison 1 consist of how many conferences?"
+    var answer1 = ["8", "10", "12", "14"]
+
+    function showItems(){
+        $("#time-left").show()
+    }
+
+});
